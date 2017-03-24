987001e9259b08e9e7bf7c7e0b1da84e908712c3
Carcer_Dun <harrisonaph@gmail.com>
2017-03-24 17:33:42 +0000

    Added in split screen, beginning of camera selection fucntionlaity and ability to move camera around

---
+++ SonoPillTest/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour 
+{
+	public float mouseSensitivity = 100.0f;
+	public float clampAngle = 80.0f;
+
+	private float rotY = 0.0f; //rotation of the y axis
+	private float rotX = 0.0f; //rotation of the x axis
+
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+	
+		rotY += mouseX * mouseSensitivity * Time.deltaTime;
+		rotX -= mouseY * mouseSensitivity * Time.deltaTime;	//use minues equals so y axis not inverted
+
+		rotX = Mathf.Clamp (rotX, -clampAngle, clampAngle);
+
+		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+		transform.rotation = localRotation;
+	}
+}
