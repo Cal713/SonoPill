1d47de6d4156a4857763e61d2ee480f4aaf39fc0
Carcer_Dun <harrisonaph@gmail.com>
2017-03-31 11:08:01 +0100

    Added in some features as required, little more work today

---
+++ SonoPillTest/Assets/Scripts/ButtonClickTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonClickTest : MonoBehaviour {
+	public Button MapButton;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = MapButton.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+	}
+	
+	void TaskOnClick()
+	{
+		Debug.Log ("Button Clicked");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
